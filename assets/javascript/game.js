70dd6eda68273eefeaf25cd430f62fcabce5d1ad
jasonjoachim <joachim.jason@gmail.com>
2017-08-12 13:42:01 -0500

    defined variables and beginning functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+
+$(document).ready(function() {
+
+var crystals = {
+    blue:{
+        name:"blue",
+        value:0
+    },
+    black:{
+        name:"black",
+        value:0
+    },
+    red:{
+        name:"red",
+        value: 0
+    },
+    green:{
+        name:"green",
+        value:0
+    }
+};
+
+var targetScore=0;
+var wins=0;
+var losses=0;
+var totalScore=0;
+var currentScore=0;
+
+
+
+//function generates a random #
+var randomNum=
+
+Math.floor(Math.random() * 100);
+
+//function to start the game
+
+function gameStart(
+    targetScore= randomNum() (min max)
+    crystals value
+    totalScore=0
+    currentScore=0
+)
